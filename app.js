d6c91b42f57f53abbd6e3e561a07291c941d06b2
holligans <alliedcodeinc@gmail.com>
2023-10-08 16:17:28 +0000

    faking task with JSON

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
-const express = require('express');
-const tasks = require('./routes/tasks');
+const express = require("express");
+const tasks = require("./routes/tasks");
 const app = express();
 
 // middlewares
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.json());
 
 // routes
-app.use('/api/v1/tasks', tasks);
+app.use("/api/v1/tasks", tasks);
 
 app.get("/hello", (req, res) => {
-    res.status(200).send("Task Manager");
-})
+  res.status(200).send("Task Manager");
+});
 
-
-const PORT = 8000;
+const PORT = 5000;
 app.listen(PORT, () => console.log(`Server listening to port: ${PORT}...`));
--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,16 +1,19 @@
+const tasks = require("../db/tasks.json");
+
 const getAllTasks = (req, res) => {
-    res.status(200).send("All Tasks");
-}
+  res.status(200).json({ tasks });
+};
 const createTask = (req, res) => {
-    res.status(200).send(req.body);
-}
+  console.log("hisdsds");
+  res.status(200).send(req.body);
+};
 const getTask = (req, res) => {
-    res.status(200).send("Get Task");
-}
+  res.status(200).send("Get Task");
+};
 const updateTask = (req, res) => {
-    res.status(200).send("Update Task");
-}
+  res.status(200).send("Update Task");
+};
 const deleteTask = (req, res) => {
-    res.status(200).send("Delete Task");
-}
-module.exports = {getAllTasks,createTask,getTask,updateTask,deleteTask};
+  res.status(200).send("Delete Task");
+};
+module.exports = { getAllTasks, createTask, getTask, updateTask, deleteTask };
